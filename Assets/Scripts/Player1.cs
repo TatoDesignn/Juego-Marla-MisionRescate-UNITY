99ec6a0db6669fe61e194e8948c936d4774733af
iSekkachi <santiago.banol@upb.edu.co>
2024-03-05 12:43:58 -0500

    idk

---
+++ Assets/Scripts/Player1.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody rb;
+
+    [Space]
+    [Header("Configuracion de Movimiento")]
+    [SerializeField] private float speed;
+    [SerializeField] private float mouseSensitivity;
+
+    [Header("Variables locales")]
+    private float xRotation;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        Camera.main.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+        
+        Vector3 move = transform.right * x + transform.forward * z;
+        rb.MovePosition(rb.position + move * speed * Time.fixedDeltaTime);
+    }
+}

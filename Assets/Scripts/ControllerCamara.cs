233dfe4f92785e3bbc48f820772afb97502cb89a
TATO DESIGN <santi1404@hotmail.com>
2024-03-23 18:45:43 -0500

    Camara puzzle finish

--- Assets/Scripts/ControllerCamara.cs
+++ Assets/Scripts/ControllerCamara.cs
@@ -5,109 +5,45 @@ using UnityEngine;
 public class ControllerCamara : MonoBehaviour
 {
     [Space]
-    [Header("Configuracion de las camaras:")]
-    [SerializeField] private GameObject vigilancia1;
-    [SerializeField] private GameObject vigilancia2;
-    [SerializeField] private GameObject vigilancia3;
-    [SerializeField] private GameObject vigilancia4;
+    [Header("Configuración de las cámaras:")]
+    [SerializeField] private GameObject[] camaras; 
     [SerializeField] private float velocidad;
 
     [Space]
     [Header("Variables locales:")]
-    private bool camara1;
-    private bool camara2;
-    private bool camara3;
-    private bool camara4;
-
+    private int camaraActiva = -1; 
 
     void Start()
     {
-        camara1 = false;
-        camara2 = false;
         Cursor.lockState = CursorLockMode.Confined;
     }
 
     void Update()
     {
-        if(camara1)
+        if (camaraActiva != -1) 
         {
             if (Input.GetKey(KeyCode.A))
             {
-                vigilancia1.transform.Rotate(Vector3.up, -velocidad * Time.deltaTime);
+                camaras[camaraActiva].transform.Rotate(Vector3.up, -velocidad * Time.deltaTime);
             }
-
             else if (Input.GetKey(KeyCode.D))
             {
-                vigilancia1.transform.Rotate(Vector3.up, velocidad * Time.deltaTime);
-            }
-        }
-        else if(camara2)
-        {
-            if (Input.GetKey(KeyCode.A))
-            {
-                vigilancia2.transform.Rotate(Vector3.up, -velocidad * Time.deltaTime);
-            }
-
-            else if (Input.GetKey(KeyCode.D))
-            {
-                vigilancia2.transform.Rotate(Vector3.up, velocidad * Time.deltaTime);
-            }
-        }
-        else if (camara3)
-        {
-            if (Input.GetKey(KeyCode.A))
-            {
-                vigilancia3.transform.Rotate(Vector3.up, -velocidad * Time.deltaTime);
-            }
-
-            else if (Input.GetKey(KeyCode.D))
-            {
-                vigilancia3.transform.Rotate(Vector3.up, velocidad * Time.deltaTime);
-            }
-        }
-        else if (camara4)
-        {
-            if (Input.GetKey(KeyCode.A))
-            {
-                vigilancia4.transform.Rotate(Vector3.up, -velocidad * Time.deltaTime);
-            }
-
-            else if (Input.GetKey(KeyCode.D))
-            {
-                vigilancia4.transform.Rotate(Vector3.up, velocidad * Time.deltaTime);
+                camaras[camaraActiva].transform.Rotate(Vector3.up, velocidad * Time.deltaTime);
             }
         }
     }
 
-    public void CamaraUno()
+    public void ActivarCamara(int indice)
     {
-        camara1 = true;
-        camara2 = false; 
-        camara3 = false;
-        camara4 = false;
+        if (indice >= 0 && indice < camaras.Length)
+        {
+            camaraActiva = indice;
+        }
     }
 
-    public void CamaraDos()
-    {
-        camara1 = false;
-        camara2 = true;
-        camara3 = false;
-        camara4 = false;
-    }
+    public void CamaraUno() { ActivarCamara(0); }
+    public void CamaraDos() { ActivarCamara(1); }
+    public void CamaraTres() { ActivarCamara(2); }
+    public void CamaraCuatro() { ActivarCamara(3); }
 
-    public void CamaraTres()
-    {
-        camara1 = false;
-        camara2 = false;
-        camara3 = true;
-        camara4 = false;
-    }
-
-    public void CamaraCuatro()
-    {
-        camara1 = false;
-        camara2 = false;
-        camara3 = false;
-        camara4 = true;
-    }
 }

e09a988f96b0f96d6ea4cbc0f0d156e8ea3fa66c
iSekkachi <santiago.banol@upb.edu.co>
2024-04-21 23:50:27 -0500

    Yeah so, Bugs Fixed, I guess

--- Assets/Scripts/AirConPuzzle/RealPipe.cs
+++ Assets/Scripts/AirConPuzzle/RealPipe.cs
@@ -5,17 +5,16 @@ using UnityEngine;
 
 public class RealPipe : MonoBehaviour
 {
-    [SerializeField] Vector3[] MyPositions = new Vector3[4];
-    [SerializeField] Vector3[] MyRotations = new Vector3[4];
+    [SerializeField] Transform[] MyPositions = new Transform[4];
     private int Stage = 0;
 
 
     public void Rotate()
     {
-        this.transform.DOMove(MyPositions[Stage], 0.2f);
-        this.transform.DORotate(MyRotations[Stage], 0.3f);
+        this.transform.DOMove(MyPositions[Stage].position, 0.2f);
+        this.transform.DORotate(MyPositions[Stage].eulerAngles, 0.3f);
         Stage++;
-        if (Stage == MyRotations.Length)
+        if (Stage == MyPositions.Length)
             Stage = 0;
     }
 }

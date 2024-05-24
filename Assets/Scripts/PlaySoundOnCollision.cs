eb9ee238182449dd7ba76744f1e31a1ebc8c9e74
Alejandroguerra4915 <alejandro.guerrab@upb.edu.co>
2024-05-24 18:36:13 -0500

    sonidos ya si

--- Assets/Scripts/PlaySoundOnCollision.cs
+++ Assets/Scripts/PlaySoundOnCollision.cs
@@ -4,32 +4,31 @@ using UnityEngine;
 
 public class PlaySoundOnCollision : MonoBehaviour
 {
-    public List<AudioClip> collisionSounds;  // Lista de sonidos para colisiones
-    public List<string> collisionTags;       // Lista de tags para colisiones
-    public List<AudioClip> triggerSounds;    // Lista de sonidos para triggers
-    public List<string> triggerTags;         // Lista de tags para triggers
+    public List<AudioClip> collisionSounds;  
+    public List<string> collisionTags;       
+    public List<AudioClip> triggerSounds;    
+    public List<string> triggerTags;         
 
-    private AudioSource audioSource;  // Referencia al componente AudioSource del jugador
+    private AudioSource audioSource;  
 
     void Start()
     {
-        // Obtener el componente AudioSource adjunto al jugador
+        
         audioSource = GetComponent<AudioSource>();
     }
 
-    // Método llamado cuando el jugador entra en colisión con otro collider
+    
     private void OnCollisionEnter(Collision collision)
     {
-        // Buscar si el tag del objeto colisionado está en la lista de tags de colisión
+       
         int index = collisionTags.IndexOf(collision.gameObject.tag);
         if (index != -1)
         {
-            // Reproducir el sonido correspondiente al tag
+           
             audioSource.PlayOneShot(collisionSounds[index]);
         }
     }
 
-    // Método llamado cuando el jugador entra en el trigger de otro collider
     private void OnTriggerEnter(Collider other)
     {
         // Buscar si el tag del objeto colisionado está en la lista de tags de trigger
@@ -38,8 +37,17 @@ public class PlaySoundOnCollision : MonoBehaviour
         {
             // Reproducir el sonido correspondiente al tag
             audioSource.PlayOneShot(triggerSounds[index]);
-        }
 
-        Destroy(other.gameObject);
+            // Esperar la duración del sonido antes de destruir el objeto
+            float soundDuration = triggerSounds[index].length;
+            StartCoroutine(DestroyAfterDelay(other.gameObject, soundDuration));
+        }
+    }
+
+    // Método para destruir el objeto después de un retraso
+    private IEnumerator DestroyAfterDelay(GameObject obj, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(obj);
     }
 }

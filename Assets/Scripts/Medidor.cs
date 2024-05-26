4b0d94d47c0e35df524640f291e26a6a6092dcdd
TATO DESIGN <santi1404@hotmail.com>
2024-05-26 15:11:44 -0500

    Solutions bug 3.0

--- Assets/Scripts/Medidor.cs
+++ Assets/Scripts/Medidor.cs
@@ -6,10 +6,15 @@ using UnityEngine;
 public class Medidor : MonoBehaviourPun
 {
     [SerializeField] GameObject perder;
+    [SerializeField] GameObject luces;
+    [SerializeField] GameObject camaras;
+
 
     public void PantallaPerder()
     {
         Cursor.lockState = CursorLockMode.Confined;
+        luces.SetActive(false); 
+        camaras.SetActive(false);
         Comunicador.canMove = false;
         perder.SetActive(true);
     }
